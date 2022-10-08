f89e2d18aac0b89acfac64077c69513fe637de50
DenisMakukh <114257585+DenisMakukh@users.noreply.github.com>
2022-10-08 22:02:56 +0300

    Update tz2.py

--- tz2.py
+++ tz2.py
@@ -1,10 +1,11 @@
 import datetime
 
-filename = input()
-file = open(filename,'r')
-for line in file:
-    sp = line.split()
-file.close()
+file = open(input(), 'r')
+try:
+    for line in file:
+        sp = line.split()
+finally:
+    file.close()
 sp1 = [int(i) for i in sp]
 print('Количество элементов в файле:', len(sp1))
 
@@ -42,4 +43,3 @@ print('Сумма:', _sum(sp1))
 print('Произведение:', _mult(sp1))
 b = datetime.datetime.now()
 c = b - a
-
